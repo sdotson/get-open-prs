ac9e8e2ee1ff7b7a57f300de0b2d5a93f295523e
sdotson <stu.dotson@gmail.com>
2017-09-28 11:26:39 -0400

    fix bug that hides older prs

--- getPrs.js
+++ getPrs.js
@@ -7,7 +7,7 @@ const team = require('./team.json');
 
 const getPrs = () => {
   github.authenticate({ type: 'oauth', token: process.env.GITHUB_TOKEN });
-  return github.issues.getAll({ filter: 'all' })
+  return github.issues.getAll({ filter: 'all', state: 'open', per_page: 100 })
   .then((res) => {
     return res.data.filter((issue) => {
       return team.includes(issue.user.login) && issue.pull_request;
